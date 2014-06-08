86d654db13196ee09bb2952c9ce3b71111acd043
abe545 <abe545@gmail.com>
2014-06-08 11:22:13 -0400

    Removed dependency on VS. Fixes #5

--- webapp.js
+++ webapp.js
@@ -2,9 +2,8 @@
 module.exports = {
   // an object that defines the schema for configuration
   config: {
-	projectFile: { type: String, default: '' },
-    vsPath:      { type: String, default: '' },
-    vsVersion:   { type: String, default: 'custom' },
+    projectFile: { type: String, default: '' },
+    netVersion:  { type: String, default: 'whatever', enum: ['1.0', '1.1', '2.0', '3.0', '3.5', '4.0', 'whatever'] },
     targets:    [{ type: String }]
   }
 }
--- worker.js
+++ worker.js
@@ -1,83 +1,92 @@
 var path = require('path')
   , fs = require('fs-extra')
   , childProc = require('child_process')
+  , async = require('async')
   , _ = require('lodash');
 
-function msbuild(context, config, vsvars, done) {
-  if (vsvars) {
-    var screen = 'msbuild ' + config.projectFile;
-    var args = [
-          '/c',
-          vsvars,
-          '&',
-          'msbuild',
-          config.projectFile
-        ];
-        
-    if (config.targets && config.targets.length) {
-      var targets = '/t:';
-      targets += _.reduce(config.targets, function (a, t) { return a + ';' + t; });
-      args.push(targets);
-      screen += ' ' + targets;
-    }
-        
-    context.cmd({
-      cmd: {
-        command: 'cmd',
-        args: args,
-        screen: screen
+function parseBuild(context, config, done) {
+  var screen = 'msbuild';
+  var args = [];
+  if (config.projectFile) {
+    args.push(config.projectFile);
+    screen += ' ' + config.projectFile;
+  }
+  
+  if (config.targets && config.targets.length) {
+    var targets = '/t:';
+    targets += _.reduce(config.targets, function (a, t) { return a + ';' + t; });
+    args.push(targets);
+    screen += ' ' + targets;
+  }
+  
+  if (config.netVersion && config.netVersion != 'whatever') {
+    findmsbuild(config.netVersion, 'Framework64', function (err, fullpath) {
+      if (err || !fullpath) {
+        findmsbuild(config.netVersion, 'Framework', function (err, fullpath) {
+          if (err) {
+            done(err);
+          } else {
+            msbuild(context, fullpath, args, screen, done);
+          }
+        });
+      } else {
+        msbuild(context, fullpath, args, screen, done);
       }
-    }, done);
+    });
+  } else {
+    msbuild(context, 'msbuild', args, screen, done);
   }
-  else {
-    context.cmd({ 
-      cmd: { command: 'msbuild', args: [config.projectFile] }
-    }, done);
-  }
+}
+
+function findmsbuild(version, framework, callback) {
+  var root = process.env.windir + '\\Microsoft.NET\\' + framework;
+  async.waterfall([
+    function (next) {
+      fs.readdir(root, next);
+    },
+    function (files, next) {
+      var dir = _.find(files, function(f) {
+        return f.indexOf('v' + version) >= 0;
+      });
+      
+      if (dir) {
+        next(null, root + '\\' + dir);
+      } else {
+        next('msbuild could not be found for .NET ' + version);
+      }
+    },
+    function (dir, next) {
+      var msbuild = dir + '\\msbuild.exe';
+      fs.exists(msbuild, function(exists) {
+        if (exists) {
+          next(null, msbuild);
+        } else {
+          next('msbuild could not be found for .NET ' + version);
+        }
+      });      
+    }
+  ], callback);
+}
+
+function msbuild(context, path, args, screen, done) {
+  context.cmd({
+    cmd: {
+      command: path,
+      args: args,
+      screen: screen
+    }
+  }, done);
 }
   
 module.exports = {
   init: function (config, job, context, cb) {
     config = config || {}
     var ret = {
-      env: {},
-      vsvars: '',
       prepare: function (context, done) {   
-        if (!config.projectFile) {
-          done(null, false);
-        } else {
-          msbuild(context, config, ret.vsvars, done);
-        }
+        parseBuild(context, config, done);
       }
     };
-      
-    if (config.vsVersion && config.vsVersion !== 'custom') {
-      var vcDir;          
-      switch (config.vsVersion) {
-        case '2013':
-          vcDir = process.env.VS120COMNTOOLS;
-          break;
-            
-        case '2012':
-          vcDir = process.env.VS110COMNTOOLS;
-          break;
-            
-        case '2010':
-          vcDir = process.env.VS100COMNTOOLS;
-          break;
-      }
-          
-      if (!vcDir) {
-        return cb('Visual Studio ' + config.vsVersion + ' specified, but it is not installed.');
-      }
-          
-      ret.vsvars = path.join(vcDir, '..', '..', 'vc', 'vcvarsall.bat');
-      cb(null, ret);
-    } else if (config.vsPath) {
-      ret.vsvars = path.join(config.vsPath, 'vc', 'vcvarsall.bat');
-      cb(null, ret);
-    } else {
-      cb(null, ret);
-    }
+    
+    cb(null, ret);
   }
 }
